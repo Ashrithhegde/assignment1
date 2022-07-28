baf319b2df302a2b9b42a3b422994059e30d9f1d
Ashrithhegde <110158419+Ashrithhegde@users.noreply.github.com>
2022-07-28 13:29:57 +0530

    Create Assignment1.js

---
+++ Assignment1.js
@@ -0,0 +1,34 @@
+question1.code
+
+let names=["Sehwag","Sachin","Gambir","Dhoni","Virat"];
+for(let i=4;i<names.length;i--){
+    console.log(names[i]);  
+}
+
+question2.code
+let country=[{
+        name:"Newzealand",
+        capital:"Wellington",
+        population:1840
+    },
+    {
+        name:"India",
+        capital:"Delhi",
+        population:767
+    },
+    {
+        name:"china",
+        capital:"beijing",
+        population:788
+    },
+    {
+        name:"japan",
+        capital:"tokyo",
+        population:999
+    }
+];
+for(let i=0;i<country.length;i++){
+    console.log(country[i]);
+}
+
+
